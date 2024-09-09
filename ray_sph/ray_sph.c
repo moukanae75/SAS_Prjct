aad285ffeab6dfb517740468e7fd98b610c841e5
unknown <moukanae75@gmail.com>
2024-09-09 18:21:25 +0200

    exe10

---
+++ ray_sph/ray_sph.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h> 
+
+
+#define M_PI 3.14159265358979323846
+
+int main() {
+    double rayon; 
+    double volume; 
+
+    system("cls");
+    printf("Entrez le rayon de la sphere : ");
+    scanf("%lf", &rayon);
+
+    
+    volume = (4.0 / 3.0) *  M_PI * pow(rayon, 3);
+
+    
+    printf("Le volume de la sphere est : %.2lf\n", volume);
+
+    return 0;
+}
